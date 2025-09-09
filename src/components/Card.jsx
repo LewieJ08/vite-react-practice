4e9cfaf8ad6947b9e1a0b856d1ff954a8398cf8d
LewieJ08 <lewiej08@gmail.com>
2025-09-09 14:50:42 +0100

    add button to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,7 @@ function Card(props) {
         <div className="card">
             <h1>{props.cardName}</h1>
             <p>{props.description}</p>
+            <button className="btn">Button</button>
         </div>
     )
 }
