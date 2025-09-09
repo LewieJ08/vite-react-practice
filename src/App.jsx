b2bd5c0e6829282701747eca5ce63cdceafbfc01
LewieJ08 <lewiej08@gmail.com>
2025-09-09 15:03:25 +0100

    feat: add cardData object for Card components

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,26 @@ import "./App.css"
 import Card from "./components/Card"
 
 function App() {
+  const cardData = {
+    card1: {
+      cardName: "John Doe",
+      description: "Software Engineer"
+    },
+    card2: {
+      cardName: "Merry Cruz",
+      description: "Marketing Expert"
+    },
+    card3: {
+      cardName: "Jason Oliver",
+      description: "Web Designer"
+    }
+  }
+  
   return (
     <div className='card-container'>
-      <Card cardName={"Card 1"} description={"Description 1"} />
-      <Card cardName={"Card 2"} description={"Description 2"} />
-      <Card cardName={"Card 3"} description={"Description 3"} />
+      <Card {...cardData.card1} />
+      <Card {...cardData.card2} />
+      <Card {...cardData.card3} />
     </div>
   )
 }
