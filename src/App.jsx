73e27a3e5e9ca595168af53fce4071cf1746cc30
LewieJ08 <lewiej08@gmail.com>
2025-09-10 20:52:14 +0100

    feat import CardCounter component into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 import "./App.css"
-import Card from "./components/Card"
+import CardCounter from "./components/CardCounter"
 
 function App() {
-  const cardData = {
-    card1: {
-      cardName: "John Doe",
-      description: "Software Engineer"
-    },
-    card2: {
-      cardName: "Merry Cruz",
-      description: "Marketing Expert"
-    },
-    card3: {
-      cardName: "Jason Oliver",
-      description: "Web Designer"
-    }
-  }
-  
   return (
     <div className='card-container'>
-      <Card {...cardData.card1} />
-      <Card {...cardData.card2} />
-      <Card {...cardData.card3} />
+      <CardCounter />
     </div>
   )
 }
