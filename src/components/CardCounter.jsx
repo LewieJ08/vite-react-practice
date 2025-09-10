d7702d1ad0521f868cd119f711d5c25d87f28756
LewieJ08 <lewiej08@gmail.com>
2025-09-10 20:51:48 +0100

    feat: add CardCounter component

---
+++ src/components/CardCounter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function CardCounter() {
+    const [count, setCount] = useState(0)
+    const increment = () => {setCount(count + 1)}
+    const decrement = () => {setCount(count - 1)}
+
+    return (
+        <div>
+            <h2>Card Counter</h2>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increase</button>
+            <button onClick={decrement}>Decrease</button>
+        </div>
+    );
+};
+
+export default CardCounter;
